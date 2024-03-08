6b0450986325a654bcad97e2b745bd7813df6e85
shivansh113 <shivanshd@gmail.com>
2024-03-08 11:55:30 -0700

    making changes to fix

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -13,18 +13,18 @@ import java.util.ArrayList;
 
 public class CustomListTest {
 
-    private CustomList list;
-
-    @Before
-    public void createList() {
-        list = new CustomList(null, new ArrayList<City>());
-    }
+    private ArrayList<City> citylist = new ArrayList<>();
 
     @Test
     public void addCityTest() {
-        int listSize = list.getCount();
-        list.addCity(new City("Halifax", "NS"));
-        assertEquals(list.getCount(), listSize+1);
+        CustomList list = new CustomList(null, citylist);
+        City city = new City("Halifax", "NS");
+        list.addCity(city);
+        assertEquals(1, list.getCount());
+    }
+
+    @Test
+    public void testHasCity() {
     }
 
 
